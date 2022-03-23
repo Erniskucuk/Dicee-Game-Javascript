d29d0d9aac98892a393bfc59d5e202bc7afa96db
Erniskucuk <66641659+Erniskucuk@users.noreply.github.com>
2022-03-23 10:11:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Dicee</title>
-    <link rel="stylesheet" href="styles.css">
-    <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Lobster" rel="stylesheet">
+// random broj izmedju 1 i 6
+var randomNumber1 = Math.floor(Math.random() * 6)+ 1;
+//ubacivanje slika i vezanje za random broj
+var diceImg1 = "images/dice" + randomNumber1 + ".png";
+//DOM vezivanje slika
+document.querySelectorAll("img")[0].setAttribute("src", diceImg1);
 
-  </head>
-  <body>
+// random broj izmedju 1 i 6 za igraca 2
+var randomNumber2 = Math.floor(Math.random() * 6)+ 1;
+//ubacivanje slika i vezanje za random broj igrac 2
+var diceImg2 = "images/dice" + randomNumber2 + ".png";
+//DOM vezivanje slika i generisinje za igraca 2
+document.querySelectorAll("img")[1].setAttribute("src", diceImg2);
 
-    <div class="container">
-      <h1>Refresh Me</h1>
+// generisanje pobjednika
 
-      <div class="dice">
-        <p>Player 1</p>
-        <img class="img1" src="images/dice6.png">
-      </div>
+   
+// igrac 1 pobjednik
+if ( randomNumber1 > randomNumber2){
+    //dodavanje texta za pobjednika igraca 1
+    document.querySelector("h1").textContent = " Player 1 Wins"
+    //igrac 2 pobjednik
+} else if (randomNumber1 < randomNumber2){
+    //dodavanje texta za pobjednika igraca 2
+    document.querySelector("h1").textContent = " Player 2 Wins"
 
-      <div class="dice">
-        <p>Player 2</p>
-        <img class="img2" src="images/dice6.png">
-      </div>
+    //generisanje za nerjeseno
+} else{
+    
+        document.querySelector("h1").textContent = " Draw"
+}
+//reload button 
+const reloadButton = document.querySelector('#btn');
+function reload(){
+    reload = location.reload();
 
-    </div>
-    <div class="refBtn">
-      <button id="btn" type="submit" onClick="refresh()">ROLL</button>
-    </div>
+}
+reloadButton.addEventListener("click", reload, false);
 
 
-
-<script src="index.js" charset="utf-8"></script>
-  </body>
-
-  <footer>
-     🎲 <a href="https://github.com/Erniskucuk"> Ernis Kucuk</a> 🎲 
-  </footer>
-</html>
